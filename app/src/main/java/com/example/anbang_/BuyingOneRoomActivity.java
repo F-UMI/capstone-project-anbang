8e9d3aef5ea6f2efd9cb57afc85c96153c81e690
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-03-17 02:08:37 +0900

    :sparkles: feat: grpc Client 구현을 위한 클래스와 의존성 추가

--- app/src/main/java/com/example/anbang_/BuyingOneRoomActivity.java
+++ app/src/main/java/com/example/anbang_/BuyingOneRoomActivity.java
@@ -2,9 +2,6 @@ package com.example.anbang_;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
